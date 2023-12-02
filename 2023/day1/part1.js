ce53a871c94d4243768a2051b676097a842c25ba
Ari <me@imaperson.dev>
2023-12-02 02:27:38 -0500

    continuity fix

--- 2023/day1/part1.js
+++ 2023/day1/part1.js
@@ -8,8 +8,10 @@ pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet`;
 const input = fs.readFileSync("./input.txt", "utf-8"); // https://adventofcode.com/2023/day/1/input
+treb7uchet`.split(/\r?\n/g);
+const input = fs.readFileSync("./input.txt", "utf-8").split(/\r?\n/g);
 
-const total = input.split("\n").reduce((acc, line) => {
+const total = input.reduce((acc, line) => {
     line = line.replace(/[^\d]/gi, "");
     return acc + +(line[0] + line[line.length - 1]);
 }, 0);
