90f8c39f76adb415e8c41d6d248d904a9ab25b23
Ari <me@imaperson.dev>
2023-12-04 00:46:15 -0500

    heavily optimize
    5-6 seconds -> 10-15 milliseconds 🤯:boom:

--- 2023/day4/part2.js
+++ 2023/day4/part2.js
@@ -2,6 +2,7 @@
 // imaperson1060
 
 const input = require("fs").readFileSync("./input.txt", "utf-8").split(/\r?\n/g);
+console.time();
 
 let cards = input.map(line => ({ winningNums: line.split(": ")[1].split(" | ")[0], card: line.split(": ")[1].split(" | ")[1] }));
 cards.map(card => card.winningNums = card.winningNums.split(" ").map(num => parseInt(num)).filter(num => !isNaN(num)));
@@ -10,10 +11,10 @@ cards.map(card => card.card = card.card.split(" ").map(num => parseInt(num)).fil
 let copies = new Array(cards.length).fill(1);
 cards.forEach((card, cardIndex) => {
     let matchingNums = 0;
-    for (let i = 0; i < copies[cardIndex]; i++) card.card.forEach(num => matchingNums += card.winningNums.includes(num));
-    for (let i = 0; i < copies[cardIndex]; i++)
-        for (let i = 1; i <= matchingNums / copies[cardIndex]; i++) copies[cardIndex + i]++;
+    card.card.forEach(num => matchingNums += card.winningNums.includes(num) * copies[cardIndex]);
+    for (let i = 1; i <= matchingNums / copies[cardIndex]; i++) copies[cardIndex + i] += copies[cardIndex];
 });
-copies = copies.filter(num => !isNaN(num)).reduce((acc, num) => acc + num);
+copies = copies.reduce((acc, num) => acc + num);
 
 console.log(copies);
+console.timeEnd();
