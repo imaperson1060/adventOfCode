6bd3070186cb29ea4f25a167bc17c60c6ef6be2d
Ari <me@imaperson.dev>
2023-12-09 00:37:17 -0500

    2023 day 9
    this was a pretty fun puzzle. i like how it focuses on logic over math, unlike some previous ones. the part 2 solution here was just part 1 but with 5 characters changed.
    
    part 1 - rank 6059 (31:41)
    part 2 - rank 5264 (34:49)

---
+++ 2023/day9/part2.js
@@ -0,0 +1,19 @@
+// https://adventofcode.com/2023/day/9
+// imaperson1060
+
+const input = require("fs").readFileSync("./input.txt", "utf-8").split(/\r?\n/g);
+
+console.log(input.map(line => {
+    line = line.split(" ").map(num => +num);
+    let layers = [ line ];
+
+    while (layers.at(-1).some(num => num)) {
+        layers.push([]);
+        for (let i = 1; i < layers.at(-2).length; i++) layers.at(-1).push(layers.at(-2)[i] - layers.at(-2)[i - 1]);
+    }
+
+    layers[layers.length - 1] = 0;
+    for (let i = layers.length - 2; i >= 0; i--) layers[i] = layers[i][0] - layers[i + 1];
+
+    return layers[0];
+}).reduce((acc, num) => acc + num));
