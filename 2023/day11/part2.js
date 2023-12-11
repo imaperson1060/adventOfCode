07098b762a2328fbd0c70479d6342fd615bebe95
Ari <me@imaperson.dev>
2023-12-11 16:08:08 -0500

    optimize 2023 day 11
    damn it im really stupid

--- 2023/day11/part2.js
+++ 2023/day11/part2.js
@@ -15,15 +15,7 @@ for (let i = 0; i < adjustedMap[0].length; i++) {
 let positions = [];
 for (let i = 0; i < adjustedMap.length; i++) { for (let j = 0; j < adjustedMap[i].length; j++) { if (adjustedMap[i][j] == "#") positions.push([i, j]); } }
 let pairs = [];
-let progress = 0;
-for (let i = 0; i < positions.length; i++) {
-    console.log(((progress / Math.pow(positions.length, 2)) * 100).toFixed(2) + "% done (" + progress, "/", Math.pow(positions.length, 2) + ")");
-    for (let j = 0; j < positions.length; j++) {
-        progress++;
-        if (i != j && !pairs.filter(pair => pair.indexOf(positions[i]) != -1 && pair.indexOf(positions[j]) != -1).length)
-            pairs.push([ positions[i], positions[j] ]);
-    }
-}
+for (let i = 0; i < positions.length; i++) { for (let j = 0; j < positions.length; j++) { if (i != j) pairs.push([ positions[i], positions[j] ]); } }
 
 console.log(pairs.map(pair => {
     let [ y1, x1 ] = pair[0], [ y2, x2 ] = pair[1];
@@ -52,4 +44,4 @@ console.log(pairs.map(pair => {
     }
 
     return deltaX + deltaY;
-}).reduce((acc, x) => acc + x));
+}).reduce((acc, x) => acc + x) / 2);
