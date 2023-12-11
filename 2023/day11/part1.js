0baa446b726492b4998d19bc45250f3cbc375eb9
Ari <me@imaperson.dev>
2023-12-11 16:08:08 -0500

    optimize 2023 day 11
    damn it im really stupid

--- 2023/day11/part1.js
+++ 2023/day11/part1.js
@@ -15,14 +15,6 @@ for (let i = 0; i < adjustedMap[0].length; i++) {
 let positions = [];
 for (let i = 0; i < adjustedMap.length; i++) { for (let j = 0; j < adjustedMap[i].length; j++) { if (adjustedMap[i][j] == "#") positions.push([i, j]); } }
 let pairs = [];
-let progress = 0;
-for (let i = 0; i < positions.length; i++) {
-    console.log(((progress / Math.pow(positions.length, 2)) * 100).toFixed(2) + "% done (" + progress, "/", Math.pow(positions.length, 2) + ")");
-    for (let j = 0; j < positions.length; j++) {
-        progress++;
-        if (i != j && !pairs.filter(pair => pair.indexOf(positions[i]) != -1 && pair.indexOf(positions[j]) != -1).length)
-            pairs.push([ positions[i], positions[j] ]);
-    }
-}
+for (let i = 0; i < positions.length; i++) { for (let j = 0; j < positions.length; j++) { if (i != j) pairs.push([ positions[i], positions[j] ]); } }
 
-console.log(pairs.map(pair => Math.abs(pair[0][0] - pair[1][0]) + Math.abs(pair[0][1] - pair[1][1])).reduce((acc, x) => acc + x));
+console.log(pairs.map(pair => Math.abs(pair[0][0] - pair[1][0]) + Math.abs(pair[0][1] - pair[1][1])).reduce((acc, x) => acc + x) / 2);
