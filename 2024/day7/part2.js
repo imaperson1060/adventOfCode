fa46f33a37abb1681c4183c23c6fc577ac0eef8d
Ari <me@imaperson.dev>
2024-12-07 02:16:31 -0500

    2024 day 7
    not only did i start late (~12:40) but my dumb ass decided that it would be a good idea to reinvent the binary counter, which then led to the trinary(??) counter in part 2. pain. horrible pain. i was literally over a hundred trillion off on my first attempt because my code didn't function. aoc weekends smh.
    
    also my code for part 2 is SLOW. at least i can still measure the runtime in single digit seconds, but it's doing so many calculation omg. is there even a way to do this that _isn't_ bruteforce?
    
    part 1 - rank 9851 (1:25:49 -> ~00:45)
    part 2 - rank 10376 (2:06:27 -> ~1:26)
    
    crazy how i took 45 minutes to do part 2 and still was that close in rank to part 1.

---
+++ 2024/day7/part2.js
@@ -0,0 +1,35 @@
+// https://adventofcode.com/2024/day/7
+// imaperson1060
+
+const input = require("fs").readFileSync("./input.txt", "utf-8").split(/\r?\n/g);
+
+function test(expected, equation, ops) {
+    let total = equation[0];
+    for (let i = 0; i < ops.length; i++)
+        total = ops[i] === "+" ? total + equation[i + 1] : ops[i] == "*" ? total * equation[i + 1] : +(total.toString() + equation[i + 1]);
+    return total == expected;
+}
+
+let operations = input.map(line => {
+    let value = +line.split(":")[0],
+        nums = line.split(":")[1].match(/\d+/g).map(Number);
+
+    let ops = Array(nums.length - 1).fill("+");
+
+    if (test(value, nums, ops)) return ops;
+    for (let i = 0; i < 3 ** (nums.length - 1); i++) {
+        if (ops[0] == "+") ops[0] = "*";
+        else if (ops[0] == "*") ops[0] = "||";
+        else {
+            ops[0] = "+";
+            let next = 1;
+            while (ops[next] == "||") ops[next++] = "+";
+            if (ops[next] == "+") ops[next] = "*";
+            else ops[next] = "||";
+        }
+
+        if (test(value, nums, ops)) return ops;
+    }
+});
+
+console.log(operations.map((operation, i) => operation ? +input[i].split(":")[0] : undefined).filter(operation => operation).reduce((a, b) => a + b));
