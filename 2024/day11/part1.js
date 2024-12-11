6368c3df5a707aaa068f4d7a05c415d346b45d1b
Ari <me@imaperson.dev>
2024-12-11 00:45:05 -0500

    2024 day 11 part 1
    part 1: rank 4433 (16:15)
    
    part 2 coming as soon as it finishes/i optimize it so that it finishes before the heat death of the universe

---
+++ 2024/day11/part1.js
@@ -0,0 +1,18 @@
+// https://adventofcode.com/2024/day/11
+// imaperson1060
+
+const input = require("fs").readFileSync("./input.txt", "utf-8").split(" ").map(x => +x);
+
+let stones = input;
+for (let i = 0; i < 25; i++) {
+    for (let j = 0; j < stones.length; j++) {
+        if (stones[j] == 0) stones[j] = 1;
+        else if (stones[j].toString().length % 2 == 0) {
+            let stone = stones[j].toString(), mid = stone.length / 2;
+            stones[j] = +stone.substr(0, mid);
+            stones.splice(++j, 0, +stone.substr(mid));
+        } else stones[j] *= 2024;
+    }
+}
+
+console.log(stones.length)
